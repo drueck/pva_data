ce476839d6b4e6b979dfcc442005205d829f1800
David Rueck <drueck@gmail.com>
2018-10-04 19:18:06 -0700

    Start implementing new Data API

--- lib/pva_data/data.ex
+++ lib/pva_data/data.ex
@@ -1,11 +1,21 @@
 defmodule PVAData.Data do
   use GenServer
 
+  alias PVAData.Divisions.Division
+
   def start_link(opts \\ []) do
     GenServer.start_link(__MODULE__, :ok, opts)
   end
 
   def init(:ok) do
-    {:ok, %{}}
+    {:ok, %{divisions: %{}}}
+  end
+
+  def put_division(pid \\ __MODULE__, %Division{} = division) do
+    GenServer.cast(pid, {:put_division, division})
+  end
+
+  def handle_cast({:put_division, division}, %{divisions: divisions} = state) do
+    {:noreply, %{state | divisions: Map.put(divisions, division.name, division)}}
   end
 end
--- test/pva_data/data_test.exs
+++ test/pva_data/data_test.exs
@@ -2,9 +2,20 @@ defmodule PVAData.DataTest do
   use ExUnit.Case, async: true
 
   alias PVAData.Data
+  alias PVAData.Divisions.Division
 
   setup do
-    data = start_supervised!(Data)
-    %{data: data}
+    %{server: start_supervised!(Data)}
+  end
+
+  describe "put_division/2" do
+    test "it sets or replaces the given division", %{server: server} do
+      division = %Division{name: "Test Division", standings: [], matches: []}
+
+      assert :ok = Data.put_division(server, division)
+
+      assert %{divisions: divisions} = :sys.get_state(server)
+      assert Map.get(divisions, division.name) == division
+    end
   end
 end
