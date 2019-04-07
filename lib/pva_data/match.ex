81942ce355a7a1b47cbd2bcc2ce9740610309ba0
David Rueck <drueck@gmail.com>
2019-04-06 21:35:58 -0700

    Add new Match struct

--- lib/pva_data/match.ex
+++ lib/pva_data/match.ex
@@ -1,3 +1,3 @@
 defmodule PVAData.Match do
-  defstruct [:date, :time, :home, :visitor, :location, :ref, :division, :set_results]
+  defstruct [:date, :time, :home, :visitor, :location, :ref, :division, set_results: []]
 end
