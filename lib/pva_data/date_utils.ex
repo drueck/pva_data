4cbb57011fb4f79cc9b424c739b7ad1157120279
David Rueck <drueck@focusvision.com>
2019-04-19 16:03:14 -0700

    Move parse_date and parse_time into DateUtils and add tests

--- lib/pva_data/date_utils.ex
+++ lib/pva_data/date_utils.ex
@@ -1,4 +1,41 @@
 defmodule PVAData.DateUtils do
+  def parse_date(date_string) do
+    ~r/(?<month>\d{1,2})\/(?<day>\d{1,2})/
+    |> Regex.named_captures(date_string)
+    |> case do
+      nil ->
+        nil
+
+      %{"month" => month_string, "day" => day_string} ->
+        month = String.to_integer(month_string)
+        day = String.to_integer(day_string)
+        year = guess_year(month, Date.utc_today())
+
+        case Date.new(year, month, day) do
+          {:ok, date} -> date
+          _ -> nil
+        end
+    end
+  end
+
+  def parse_time(time_string) do
+    ~r/(?<hour>\d{1,2}):(?<minutes>\d{1,2})/
+    |> Regex.named_captures(time_string)
+    |> case do
+      nil ->
+        nil
+
+      %{"hour" => hour_string, "minutes" => minutes_string} ->
+        hour = String.to_integer(hour_string) + 12
+        minutes = String.to_integer(minutes_string)
+
+        case Time.new(hour, minutes, 0) do
+          {:ok, time} -> time
+          _ -> nil
+        end
+    end
+  end
+
   def guess_year(referenced_month, today) do
     case referenced_month do
       m when m in 1..2 ->
--- lib/pva_data/schedules_scraper.ex
+++ lib/pva_data/schedules_scraper.ex
@@ -16,8 +16,8 @@ defmodule PVAData.SchedulesScraper do
         |> Enum.map(&Meeseeks.text/1)
 
       [location, ref] = split_location_and_ref(location_and_ref)
-      date = parse_date(date_string)
-      time = parse_time(time_string)
+      date = DateUtils.parse_date(date_string)
+      time = DateUtils.parse_time(time_string)
 
       %Match{
         date: date,
@@ -30,43 +30,6 @@ defmodule PVAData.SchedulesScraper do
       }
     end)
     |> (fn matches -> {:ok, matches} end).()
-  end
-
-  defp parse_date(date_string) do
-    ~r/(?<month>\d{1,2})\/(?<day>\d{1,2})/
-    |> Regex.named_captures(date_string)
-    |> case do
-      nil ->
-        nil
-
-      %{"month" => month_string, "day" => day_string} ->
-        month = String.to_integer(month_string)
-        day = String.to_integer(day_string)
-        year = DateUtils.guess_year(month, Date.utc_today())
-
-        case Date.new(year, month, day) do
-          {:ok, date} -> date
-          _ -> nil
-        end
-    end
-  end
-
-  defp parse_time(time_string) do
-    ~r/(?<hour>\d{1,2}):(?<minutes>\d{1,2})/
-    |> Regex.named_captures(time_string)
-    |> case do
-      nil ->
-        nil
-
-      %{"hour" => hour_string, "minutes" => minutes_string} ->
-        hour = String.to_integer(hour_string) + 12
-        minutes = String.to_integer(minutes_string)
-
-        case Time.new(hour, minutes, 0) do
-          {:ok, time} -> time
-          _ -> nil
-        end
-    end
   end
 
   defp split_location_and_ref(location_and_ref) do
