503442bfe386e2e759da1f13deb98d150049eed1
David Rueck <drueck@gmail.com>
2019-04-20 17:34:06 -0700

    Add TeamsScraper to get divisions and teams

---
+++ test/pva_data/teams_scraper_test.exs
@@ -0,0 +1,38 @@
+defmodule PVAData.TeamsScraperTest do
+  use ExUnit.Case, async: true
+
+  alias PVAData.{
+    TeamsScraper,
+    Division,
+    Team
+  }
+
+  describe "get_teams_by_division/1" do
+    test "returns a list of divisions with teams in each" do
+      assert {:ok, divisions} =
+               "test/fixtures/schedules.php"
+               |> File.read!()
+               |> TeamsScraper.get_teams_by_division()
+
+      expected_division_count = 8
+
+      expected_first_division = %Division{
+        name: "Coed A Thursday",
+        slug: "coed-a-thursday",
+        teams: [
+          %Team{name: "Chewblocka", slug: "chewblocka"},
+          %Team{name: "Court Jesters", slug: "court-jesters"},
+          %Team{name: "Empire Spikes Back", slug: "empire-spikes-back"},
+          %Team{name: "Group Sets", slug: "group-sets"},
+          %Team{name: "Other People's Spouses", slug: "other-peoples-spouses"},
+          %Team{name: "Pokéballs", slug: "pokeballs"},
+          %Team{name: "Pound Town", slug: "pound-town"},
+          %Team{name: "Whatever", slug: "whatever"}
+        ]
+      }
+
+      assert length(divisions) == expected_division_count
+      assert hd(divisions) == expected_first_division
+    end
+  end
+end
