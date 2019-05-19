b28386bb256974fc2b8937ba39ef05c9eadeed7d
David Rueck <drueck@gmail.com>
2019-05-19 15:59:33 -0700

    Start implementing the Data store, list use cases in README for notes

---
+++ lib/pva_data/data.ex
@@ -0,0 +1,40 @@
+defmodule PVAData.Data do
+  use GenServer
+
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  def init(:ok) do
+    {:ok, %{divisions: %{}}}
+  end
+
+  def put_division(pid \\ __MODULE__, division) do
+    GenServer.cast(pid, {:put_division, division})
+  end
+
+  def get_division(pid \\ __MODULE__, slug) do
+    GenServer.call(pid, {:get_division, slug})
+  end
+
+  def list_divisions(pid \\ __MODULE__) do
+    GenServer.call(pid, :list_divisions)
+  end
+
+  def handle_cast({:put_division, division}, %{divisions: divisions} = state) do
+    {:noreply, %{state | divisions: Map.put(divisions, division.slug, division)}}
+  end
+
+  def handle_call({:get_division, slug}, _from, %{divisions: divisions} = state) do
+    {:reply, Map.get(divisions, slug), state}
+  end
+
+  def handle_call(:list_divisions, _from, %{divisions: divisions} = state) do
+    division_list =
+      divisions
+      |> Map.values()
+      |> Enum.map(&Map.take(&1, [:name, :slug]))
+
+    {:reply, division_list, state}
+  end
+end
