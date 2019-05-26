69d142a863dd215047cac730a95f86efa692cc1e
David Rueck <drueck@gmail.com>
2019-05-25 23:15:08 -0700

    Add initial version of ScraperBot and test
    The test uses the PVAWebsite.FakeClient so it uses the fixture data.

---
+++ lib/pva_data/scraper_bot.ex
@@ -0,0 +1,45 @@
+defmodule PVAData.ScraperBot do
+  alias PVAData.Data
+
+  @pva_website Application.get_env(:pva_data, :pva_website_client)
+
+  def update_data do
+    with {:ok, divisions} <- @pva_website.get_teams_by_division(),
+         {:ok, scheduled_matches} <- @pva_website.get_scheduled_matches(),
+         {:ok, completed_matches} <- @pva_website.get_completed_matches(),
+         {:ok, division_standings} <- @pva_website.get_division_standings() do
+      divisions
+      |> Enum.each(fn division ->
+        division
+        |> add_scheduled_matches(scheduled_matches)
+        |> add_completed_matches(completed_matches)
+        |> add_standings(division_standings)
+        |> Data.put_division()
+      end)
+    end
+  end
+
+  defp add_scheduled_matches(division, all_scheduled_matches) do
+    scheduled_matches =
+      all_scheduled_matches
+      |> Enum.filter(&(&1.division == division.name))
+
+    %{division | scheduled_matches: scheduled_matches}
+  end
+
+  defp add_completed_matches(division, all_completed_matches) do
+    completed_matches =
+      all_completed_matches
+      |> Enum.filter(&(&1.division == division.name))
+
+    %{division | completed_matches: completed_matches}
+  end
+
+  defp add_standings(division, all_division_standings) do
+    division_standings =
+      all_division_standings
+      |> Enum.find(&(&1.division == division.name))
+
+    %{division | standings: division_standings.standings}
+  end
+end
---
+++ test/pva_data/scraper_bot_test.exs
@@ -0,0 +1,39 @@
+defmodule PVAData.ScraperBotTest do
+  use ExUnit.Case, async: true
+
+  alias PVAData.{
+    Data,
+    ScraperBot,
+    Division,
+    Team,
+    Match,
+    Standing,
+    SetResult
+  }
+
+  describe "#update_data/0" do
+    test "updates the data store with the latest info from the website" do
+      ScraperBot.update_data()
+
+      assert %{divisions: divisions} = :sys.get_state(Data)
+      assert Map.has_key?(divisions, "womens-aa-tuesday")
+      assert %Division{} = division = divisions["womens-aa-tuesday"]
+
+      assert division.name == "Womens AA Tuesday"
+      assert division.slug == "womens-aa-tuesday"
+
+      assert length(division.teams) == 6
+      assert length(division.standings) == 6
+      assert length(division.scheduled_matches) == 5
+      assert length(division.completed_matches) == 2
+
+      assert %Team{} = hd(division.teams)
+      assert %Match{} = hd(division.scheduled_matches)
+      assert %Standing{} = hd(division.standings)
+      assert %Match{} = completed_match = hd(division.completed_matches)
+
+      assert length(completed_match.set_results) == 3
+      assert %SetResult{} = hd(completed_match.set_results)
+    end
+  end
+end
