6efbe61e0bc1f201e126ed4674b10ce9fd57cf0f
David Rueck <drueck@gmail.com>
2020-03-28 02:14:17 -0700

    Move children into env specific config to prevent starting in test

--- config/prod.exs
+++ config/prod.exs
@@ -1,1 +1,6 @@
 use Mix.Config
+
+alias PVAData.Data
+
+config :pva_data,
+  other_children: [{Data, [name: Data]}]
--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,5 @@
 use Mix.Config
 
 config :pva_data,
-  pva_website_client: PVAData.PVAWebsite.FakeClient
+  pva_website_client: PVAData.PVAWebsite.FakeClient,
+  other_children: []
