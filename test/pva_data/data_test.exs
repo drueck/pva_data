2470cb5d2ec6e93a285224e39d434d99a0eab5fe
David Rueck <drueck@gmail.com>
2020-03-30 16:30:56 -0700

    Allow API users to drill down into all data from divisions query
    This could be a decent amount of data, but not that crazy, so I figured
    may as well make it work more like a normal graphql api, not have a
    weird restriction on the top level that you can only get a couple of
    fields.

--- test/pva_data/data_test.exs
+++ test/pva_data/data_test.exs
@@ -13,13 +13,14 @@ defmodule PVAData.DataTest do
 
   describe "put_division/2" do
     test "adds the division by its slug to the divisions map", %{server: server} do
-      coed_a_thursday = %Division{
-        name: "Coed A Thursday",
-        slug: "coed-a-thursday",
-        teams: [
-          %Team{name: "Court Jesters", slug: "court-jesters"}
-        ]
-      }
+      coed_a_thursday =
+        Division.new(
+          name: "Coed A Thursday",
+          slug: "coed-a-thursday",
+          teams: [
+            %Team{name: "Court Jesters", slug: "court-jesters"}
+          ]
+        )
 
       assert :ok = Data.put_division(server, coed_a_thursday)
 
@@ -30,10 +31,11 @@ defmodule PVAData.DataTest do
 
   describe "get_division/2" do
     test "gets the division with the given slug", %{server: server} do
-      coed_a_thursday = %Division{
-        name: "Coed A Thursday",
-        slug: "coed-a-thursday"
-      }
+      coed_a_thursday =
+        Division.new(
+          name: "Coed A Thursday",
+          slug: "coed-a-thursday"
+        )
 
       assert :ok = Data.put_division(server, coed_a_thursday)
 
@@ -42,20 +44,16 @@ defmodule PVAData.DataTest do
   end
 
   describe "list_divisions/1" do
-    test "returns the list of division names and slugs", %{server: server} do
-      [
-        %Division{name: "Coed A Thursday", slug: "coed-a-thursday"},
-        %Division{name: "Coed B Wednesday", slug: "coed-b-wednesday"}
-      ]
+    test "returns the divisions", %{server: server} do
+      coed_a_thursday = Division.new(name: "Coed A Thursday", slug: "coed-a-thursday")
+      coed_b_wednesday = Division.new(name: "Coed B Wednesday", slug: "coed-b-wednesday")
+
+      [coed_a_thursday, coed_b_wednesday]
       |> Enum.each(&Data.put_division(server, &1))
 
       divisions = Data.list_divisions(server)
 
-      assert sort_by_name(divisions) ==
-               sort_by_name([
-                 %{name: "Coed A Thursday", slug: "coed-a-thursday"},
-                 %{name: "Coed B Wednesday", slug: "coed-b-wednesday"}
-               ])
+      assert sort_by_name(divisions) == sort_by_name([coed_a_thursday, coed_b_wednesday])
     end
   end
 
