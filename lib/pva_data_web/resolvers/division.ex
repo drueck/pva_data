a2f086bf37be26ca19e816a4278930a317c8f49a
David Rueck <drueck@gmail.com>
2020-04-01 16:50:56 -0700

    Add the ability to find a division by its slug

--- lib/pva_data_web/resolvers/division.ex
+++ lib/pva_data_web/resolvers/division.ex
@@ -12,6 +12,12 @@ defmodule PVADataWeb.Resolvers.Division do
     get(division_id)
   end
 
+  def by_slug(_, %{slug: slug}, _) do
+    division = Data.get_division_by_slug(slug)
+
+    {:ok, division}
+  end
+
   defp get(id) do
     {:ok, Data.get_division(id)}
   end
--- lib/pva_data_web/schema.ex
+++ lib/pva_data_web/schema.ex
@@ -9,5 +9,11 @@ defmodule PVADataWeb.Schema do
     field :divisions, list_of(:division) do
       resolve &Resolvers.Division.all/2
     end
+
+    field :division, :division do
+      arg :slug, :string
+
+      resolve &Resolvers.Division.by_slug/3
+    end
   end
 end
