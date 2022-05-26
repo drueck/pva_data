9809e3aaa6e5ee74f6eddddbeb56ebf19f0352e2
David Rueck <drueck@gmail.com>
2022-05-26 15:21:42 -0700

    Handle missing scores, schedules, standings
    This can happen if the PVA website is in a weird inconsistent state in
    between seasons (like it currently is now). This was causing the
    genserver to crash and the data not to be updated.

--- lib/pva_data/scraper.ex
+++ lib/pva_data/scraper.ex
@@ -20,26 +20,29 @@ defmodule PVAData.Scraper do
   end
 
   defp add_scheduled_matches(division, all_scheduled_matches) do
-    scheduled_matches =
-      all_scheduled_matches
-      |> Enum.filter(&(&1.division_id == division.id))
-
-    %{division | scheduled_matches: scheduled_matches}
+    all_scheduled_matches
+    |> Enum.filter(&(&1.division_id == division.id))
+    |> case do
+      nil -> division
+      scheduled_matches -> %{division | scheduled_matches: scheduled_matches}
+    end
   end
 
   defp add_completed_matches(division, all_completed_matches) do
-    completed_matches =
-      all_completed_matches
-      |> Enum.filter(&(&1.division_id == division.id))
-
-    %{division | completed_matches: completed_matches}
+    all_completed_matches
+    |> Enum.filter(&(&1.division_id == division.id))
+    |> case do
+      nil -> division
+      completed_matches -> %{division | completed_matches: completed_matches}
+    end
   end
 
   defp add_standings(division, all_division_standings) do
-    division_standings =
-      all_division_standings
-      |> Enum.find(&(&1.division_id == division.id))
-
-    %{division | standings: division_standings.standings}
+    all_division_standings
+    |> Enum.find(&(&1.division_id == division.id))
+    |> case do
+      %{standings: standings} -> %{division | standings: standings}
+      _ -> division
+    end
   end
 end
