6ba8214ae9a686c041d1f7436984d5428fce2856
David Rueck <drueck@gmail.com>
2022-06-04 19:34:50 -0700

    If password is not required, auth middleware allows anything

--- lib/pva_data_web/middlewares/authentication.ex
+++ lib/pva_data_web/middlewares/authentication.ex
@@ -2,5 +2,12 @@ defmodule PVADataWeb.Middlewares.Authentication do
   @behaviour Absinthe.Middleware
 
   def call(%{context: %{authenticated: _}} = res, _), do: res
-  def call(res, _), do: Absinthe.Resolution.put_result(res, {:error, "not authorized"})
+
+  def call(res, _) do
+    if Application.get_env(:pva_data, :password_required) do
+      Absinthe.Resolution.put_result(res, {:error, "not authorized"})
+    else
+      res
+    end
+  end
 end
