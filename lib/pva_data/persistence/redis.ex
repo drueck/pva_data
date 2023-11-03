e8ba4856d09c371269c8c38cdff03d3a51da61aa
David Rueck <drueck@gmail.com>
2023-11-02 18:36:35 -0700

    Use Redix.command! and report/reraise exceptions
    Redix.command now returns several possible values in an :error tuple,
    all of which we basically want to report to Rollbar, and it seemed
    unnecessary to match on each type (Redix.Error, Redix.ConnectionError,
    and also the plain binary option) and report those all in different ways
    because they each have different structures.

--- lib/pva_data/persistence/redis.ex
+++ lib/pva_data/persistence/redis.ex
@@ -4,30 +4,34 @@ defmodule PVAData.Persistence.Redis do
   @state_key Application.compile_env(:pva_data, :persistence_state_key)
 
   def save_state(state) do
-    Redix.command(:redix, ["SET", @state_key, :erlang.term_to_binary(state)])
-    |> case do
-      {:ok, "OK"} ->
-        :ok
-
-      error ->
-        Rollbax.report_message(:error, "Failed to save state to redis", %{error: error})
-        raise "Failed to save state to redis"
+    try do
+      Redix.command!(:redix, ["SET", @state_key, :erlang.term_to_binary(state)])
+      :ok
+    rescue
+      e ->
+        Rollbax.report(:error, e, __STACKTRACE__)
+        reraise e, __STACKTRACE__
     end
   end
 
   def read_state do
-    Redix.command(:redix, ["GET", @state_key])
-    |> case do
-      {:ok, nil} ->
-        Rollbax.report_message(:warning, "No state was found in redis.", %{state_key: @state_key})
-        {:error, "no state available"}
+    try do
+      Redix.command!(:redix, ["GET", @state_key])
+      |> case do
+        nil ->
+          Rollbax.report_message(:warning, "No state was found in redis.", %{
+            state_key: @state_key
+          })
 
-      {:ok, serialized_state} ->
-        {:ok, :erlang.binary_to_term(serialized_state)}
+          {:error, "no state available"}
 
-      error ->
-        Rollbax.report_message(:error, "Failed to read state from redis", %{error: error})
-        raise "Failed to read state from redis"
+        serialized_state ->
+          {:ok, :erlang.binary_to_term(serialized_state)}
+      end
+    rescue
+      e ->
+        Rollbax.report(:error, e, __STACKTRACE__)
+        reraise e, __STACKTRACE__
     end
   end
 end
